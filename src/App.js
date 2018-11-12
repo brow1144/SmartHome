cf289a32865ee14207a955a0129964d09845847e
Kyle Brown <brow1144@purdue.edu>
2018-11-11 21:02:24 -0500

    Init Commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Button} from 'mdbreact';
+import axios from 'axios';
+
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+
 class App extends Component {
+
+  officeOn() {
+    axios.get('https://maker.ifttt.com/trigger/Office_On/with/key/dWo86bkjmWq7VdRJ1T8p5a')
+    .then(response => {
+      console.log(response)
+    })
+  }
+
+  officeOff() {
+    axios.get('https://maker.ifttt.com/trigger/Office_Off/with/key/dWo86bkjmWq7VdRJ1T8p5a')
+    .then(response => {
+      console.log(response)
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,15 +33,11 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+          <Button onClick={this.officeOn} color="primary">Office On</Button>
+          <Button onClick={this.officeOff} color="primary">Office Off</Button>
+
+          </header>
       </div>
     );
   }
